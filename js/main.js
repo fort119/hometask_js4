0ab3de05841f003d2b61240caa289389c007eb76
fort119 <i6316434@gmail.com>
2023-06-29 16:03:34 +0400

    ht4 is done

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ console.log(cityOfResidence);
 let favSport = prompt('Введіть улюблений вид спорту');
 console.log(favSport);
 
-let capitalCity = 'Ви живете у місті';
-let checkSport = 'Ваш улюблений спорт:';
+let capitalCity = 'Ви живете у місті: ';
+let checkSport = 'Ваш улюблений спорт: ';
 
 result = {
   age: calcYear,
@@ -21,39 +21,42 @@ if (isNaN(yearOfBirth)) {
   alert('Використайте цифри для введення свого року народження')
   result.age = 'не встанволено'
 } else if (yearOfBirth === null) {
-  alert('шкода, що Ви не захотіли вводити вік свого народження')
-  result.age = 'не встанволено'
+  alert('шкода, що Ви не захотіли ввести свій вік народження')
+  result.age = 'не вказано'
 }
 
 if (cityOfResidence === null) {
-  alert('шкода, що Ви не захотіли вводити місто свого проживання')
-  result.city = 'не встанволено'
+  alert('шкода, що Ви не захотіли ввести своє місто проживання')
+  result.city = 'не вказано'
 } else if (cityOfResidence === 'Київ') {
-  capitalCity = 'Ти живеш у столиці України - '
+  capitalCity = 'Ти живеш у столиці України'
+  result.city = '';
 } else if (cityOfResidence === 'Вашингтон') {
-  capitalCity = 'Ти живеш у столиці США - '
+  capitalCity = 'Ти живеш у столиці США'
+  result.city = '';
 } else if (cityOfResidence === 'Лондон') {
-  capitalCity = 'Ти живеш у столиці Англії - '
+  capitalCity = 'Ти живеш у столиці Англії'
+  result.city = '';
 };
 
 if (favSport === null) {
-  alert('шкода, що Ви не захотіли вводити свій улюблений вид спорту')
-  result.sport = 'не встанволено'
+  alert('шкода, що Ви не захотіли ввести свій улюблений вид спорту')
+  result.sport = 'не вказано'
 }
 
 switch (favSport) {
   case 'бокс':
     checkSport = 'Круто! Хочешь стати Майком Тайсоном?';
-    alert(`Ваш вік: ${result.age}, ${capitalCity} ${result.city}. ${checkSport}`)
+    alert('Ваш вік: ' + result.age + '\n' + capitalCity + result.city + "\n" + checkSport);
     break;
   case 'плавання':
     checkSport = 'Круто! Хочешь стати Майклом Фелпсом?';
-    alert(`Ваш вік: ${result.age}, ${capitalCity} ${result.city}. ${checkSport}`)
+    alert('Ваш вік: ' + result.age + '\n' + capitalCity + result.city + "\n" + checkSport);
     break;
   case 'гольф':
     checkSport = 'Круто! Хочешь стати Тайгером Вудсом?';
-    alert(`Ваш вік: ${result.age}, ${capitalCity} ${result.city}. ${checkSport}`)
+    alert('Ваш вік: ' + result.age + '\n' + capitalCity + result.city + "\n" + checkSport);
     break;
   default:
-    alert(`Ваш вік: ${result.age}, ${capitalCity} ${result.city}. ${checkSport} ${result.sport}`);
+    alert('Ваш вік: ' + result.age + '\n' + capitalCity + result.city + "\n" + checkSport + result.sport);
 };
